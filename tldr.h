52ac83840c01ec997b488f046b9e08f6a86f3303
szymbar15 <szymbar1513@gmail.com>
2014-11-13 20:09:49 +0100

    Used Yami's and Tycoonie's fork, finally optimised exceptions

---
+++ tldr.h
@@ -0,0 +1,22 @@
+#ifndef TLDR_H_
+#define TLDR_H_
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Couting {
+public:
+	void CourseList();
+};
+
+class FileIO {
+public:
+	string GetFileContents(const char *filename);
+};
+
+#endif
