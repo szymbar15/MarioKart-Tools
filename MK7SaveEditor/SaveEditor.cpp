56d9aeb4f6e50f6e906cab11f5347bae1c79e554
Bartłomiej Szymański <szymbar1513@gmail.com>
2014-12-04 10:03:39 +0100

    CRC32 commit

--- MK7SaveEditor/SaveEditor.cpp
+++ MK7SaveEditor/SaveEditor.cpp
@@ -1,4 +1,5 @@
-//This keeps shaping better and better.
+//A freaking mess, but it'll get fixed. Eventually.
+#include "CRC32.h"
 #include "coinfunctions.h"
 
 int main() {
@@ -8,10 +9,11 @@ int main() {
 	CountDecimalsFour dec;
 	CountHexadecimalsFour hex;
 	Export exportit;
+	CRC32 crc;
 	
 
 	const int offset=19496;
-	string text=import.GetFileContents("system0.dat");
+	string text=import.GetFileContents("test.txt");
 	string coinsstr="ABCD";
 	for (int i=offset; i<offset+4; i++) {
 		coinsstr[i-offset]=text[i];
@@ -24,14 +26,26 @@ int main() {
 	cout << coinsstr;
 	int coinamt;
 	coinamt=dec.fourbytedeccalculate(coinsstr);
-	cout << coinamt;
+	cout << endl << coinamt;
 	cin >> coinamt;
-	cout << coinamt;
+	cout << endl << coinamt;
 	coinsstr=endian.littleendian32(hex.fourbytehexcalculate(coinamt));
-	cout << coinsstr;
+	cout << endl << coinsstr;
 	for (int i=offset; i<offset+4; i++) {
 		text[i]=coinsstr[i-offset];
 	}
+	string textcrc;
+	textcrc=text.substr(0, text.size()-4);
+	long long notcomplete;
+	notcomplete=crc.crccalc(textcrc);
+	string complete;
+	complete=endian.littleendian32(hex.fourbytehexcalculate(notcomplete));
+	/*string complete=notcomplete;
+	cout << notcomplete;*/
+	//cout << endl << (int) notcomplete[0] << " " << (int) notcomplete[1] << " " << (int) notcomplete[2] << " " << (int) notcomplete[3] << endl;
+	for (int i=0; i<4; i++) {
+		text[text.size()-4+i]=complete[i];
+	}
 	//output
-    	exportit.backup(text, "edited.dat");
+    exportit.backup(text, "edited.dat");
 }
